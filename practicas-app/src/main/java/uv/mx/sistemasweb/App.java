a3b7336a668fc166b7a22bd5bbeb6bb73aa5db97
agustin123gd <agustin.gutierrez.103@gmail.com>
2021-12-13 13:53:48 -0600

    Examen agustin pregunta 4

--- practicas-app/src/main/java/uv/mx/sistemasweb/App.java
+++ practicas-app/src/main/java/uv/mx/sistemasweb/App.java
@@ -1,5 +1,7 @@
 package uv.mx.sistemasweb;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,31 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+
+        get("/", (req, res) -> {
+            return null;
+        });
+
+        get("/aprobado", (request, response) -> {
+            return "Querido santa, me gustaria sacar un 11, de no ser posible con un 10 estoy feliz.";
+        });
     }
 }
